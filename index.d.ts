5c45e3ed4fc9dfbc5c3e2c771dc8e5d1cd200d82
Quentin Correia <quentin@cirqll.nl>
2025-04-01 20:51:11 +0700

    feat: initial commit

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+﻿declare const canvas: HTMLCanvasElement | null;
+declare const ctx: CanvasRenderingContext2D;
+declare const CANVAS_WIDTH: number;
+declare const CANVAS_HEIGHT: number;
+declare enum Color {
+    White = "white",
+    Black = "black",
+    Red = "red"
+}
+declare function play(width: number, height: number, color: Color): void;
